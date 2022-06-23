e3094f0f967cf9aec6af2788fbbca42a39974321
saramonzon22 <sara.mon.quesada@gmail.com>
2022-06-23 10:24:47 +0100

    two kittens

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 'use strict'
 
-const jsNew = document.querySelector('.js-new-form'); 
-jsNew.classList.remove("collapsed");
+// const jsNew = document.querySelector('.js-new-form'); 
+// jsNew.classList.remove("collapsed");
 
 const jsList = document.querySelector('.js-list');
 
 const kittenOne = '<li class="card"> <article> <img   class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito"/> <h3 class="card_title">Anastacio</h3> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </article> </li>';
+const kittenTwo = '<li class="card"> <article> <img   class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito"/> <h3 class="card_title">Anastacio</h3> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </article> </li>';
+
+jsList.innerHTML = kittenOne + kittenTwo;
 
